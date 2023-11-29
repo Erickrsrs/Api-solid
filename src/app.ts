458ee47dc6bf4e2613bed7af51796010aef7703f
Erick Rocha <erick.r.souza@cogna.com.br>
2023-11-29 20:18:12 -0300

    chore: add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+export const prisma = new PrismaClient()
+
+prisma.user.create({ data: { name: 'Alice', email: 'alice@email.com' } })
 
 export const app = fastify()
