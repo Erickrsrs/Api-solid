e4705225a83d1a6eb28f24494821dac3a833d11e
Erick Rocha <erick.r.souza@cogna.com.br>
2023-12-01 21:11:27 -0300

    chore: use dependency inversion

--- src/api/services/register.ts
+++ src/api/services/register.ts
@@ -1,33 +1,28 @@
 import { hash } from 'bcryptjs'
-import { prisma } from '../../lib/prisma'
-import { PrismaUsersRepository } from '../repositories/prisma-users-repository'
+import { UsersRepository } from '../repositories/users-repository'
 
-interface RegisterService {
+interface IRegisterService {
   name: string
   email: string
   password: string
 }
 
-export async function registerService({
-  name,
-  email,
-  password,
-}: RegisterService) {
-  const password_hash = await hash(password, 6)
+export class RegisterService {
+  constructor(private usersRepository: UsersRepository) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: { email },
-  })
+  async execute({ name, email, password }: IRegisterService) {
+    const password_hash = await hash(password, 6)
 
-  if (userWithSameEmail) {
-    throw new Error('User with this email already exists')
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new Error('User with this email already exists')
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
