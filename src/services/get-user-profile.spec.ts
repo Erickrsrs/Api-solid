da85858783b68a8858c13a3dc8f6284293e0739a
Erick Rocha <erick.r.souza@cogna.com.br>
2023-12-03 16:42:24 -0300

    feat: Add GetUserProfileService and ResourceNotFoundError

---
+++ src/services/get-user-profile.spec.ts
@@ -0,0 +1,43 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { hash } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { GetUserProfileService } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileService
+
+describe('get user profile service', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileService(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'jons@email.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user).toMatchObject({
+      id: expect.any(String),
+      name: 'John Doe',
+      email: expect.any(String),
+      password_hash: expect.any(String),
+      created_at: expect.any(Date),
+    })
+  })
+
+  it('should not be able to get user profile with wrong id', async () => {
+    expect(async () => {
+      await sut.execute({
+        userId: 'wrong-id',
+      })
+    }).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
