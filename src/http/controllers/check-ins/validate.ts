c1f2e1f4bbf9a2880399eb499ae3e5bfc2bdd2dc
Erick Rocha <erick.r.souza@cogna.com.br>
2023-12-10 21:22:49 -0300

    feat: create gyms e2e tests

--- src/http/controllers/check-ins/validate.ts
+++ src/http/controllers/check-ins/validate.ts
@@ -1,6 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { makeValidateCheckInService } from '../../../services/factories/make-validate-check-in-service'
+import { makeValidateCheckInService } from '@/services/factories/make-validate-check-in-service'
 
 export async function validate(request: FastifyRequest, reply: FastifyReply) {
   const validateCheckInParamsSchema = z.object({
