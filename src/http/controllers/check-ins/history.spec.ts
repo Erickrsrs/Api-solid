881b88759454bafb9ca6fb42e4cdbac35f476be0
Erick Rocha <erick.r.souza@cogna.com.br>
2023-12-10 21:40:11 -0300

    chore: add checkIns tests e2e

---
+++ src/http/controllers/check-ins/history.spec.ts
@@ -0,0 +1,61 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Check-in History (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to list the history of user check-ins', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const user = await prisma.user.findFirstOrThrow()
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Gym 01',
+        description: 'Gym 01 description',
+        phone: '123456789',
+        latitude: 0,
+        longitude: 0,
+      },
+    })
+
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          user_id: user.id,
+          gym_id: gym.id,
+        },
+        {
+          user_id: user.id,
+          gym_id: gym.id,
+        },
+      ],
+    })
+
+    const response = await request(app.server)
+      .get('/check-ins/history')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkIns).toEqual([
+      expect.objectContaining({
+        user_id: user.id,
+        gym_id: gym.id,
+      }),
+      expect.objectContaining({
+        user_id: user.id,
+        gym_id: gym.id,
+      }),
+    ])
+  })
+})
